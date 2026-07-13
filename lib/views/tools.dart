3712ab8ec3be0a6f1005ace5f0e795a7790ecd16
TianTiLink <ShunsukeShoaf471@gmail.com>
2026-07-13 18:39:47 +0800

    Update tools.dart

--- lib/views/tools.dart
+++ lib/views/tools.dart
@@ -74,11 +74,8 @@ class _ToolViewState extends ConsumerState<ToolsView> {
       items: [
         const _LocaleItem(),
         const _ThemeItem(),
-        const _BackupItem(),
         if (system.isDesktop) const _HotkeyItem(),
-        if (system.isWindows) const _LoopbackItem(),
         if (system.isAndroid) const _AccessItem(),
-        const _ConfigItem(),
         const _AdvancedConfigItem(),
         const _SettingItem(),
       ],
